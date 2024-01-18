945f7fb999cc666fea24ca4f8dd582bcfbea3e35
xiaoyu <xiaoyu@sulg.top>
2024-01-18 19:32:29 +0800

    调整路径

--- src/renderer/homeWin/src/router/index.tsx
+++ src/renderer/homeWin/src/router/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const Setting = lazy(() => import("@home/pages/Setting"));
-const Home = lazy(() => import("@home/pages/Home"));
+const Setting = lazy(() => import("@homeWin/pages/Setting"));
+const Home = lazy(() => import("@homeWin/pages/Home"));
 
 export default function BaseRouter() {
   return (
-    <BrowserRouter basename="home/index.html">
+    <BrowserRouter basename="homeWin/index.html">
       <Suspense fallback={<div>loading</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
